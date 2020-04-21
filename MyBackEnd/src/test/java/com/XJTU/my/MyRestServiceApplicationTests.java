7c9483ff0e2059433cabea6205d0bafde65443ff
qiuxiaoming <1027008329@qq.com>
2020-04-21 18:10:46 +0800

    Restore deleted tests

---
+++ MyBackEnd/src/test/java/com/XJTU/my/MyRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.XJTU.my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MyRestServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
